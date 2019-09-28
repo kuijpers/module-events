b00a5ac1dc413c784e188e27e749694743c1a9cc
kuijpers <darkknightworks@gmail.com>
2019-09-28 23:27:27 +0200

    Added models and controlers for the various parts that will be handled in this module

---
+++ Entities/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Events\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [];
+}
---
+++ Entities/Timeline.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Events\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timeline extends Model
+{
+    protected $fillable = [];
+}
